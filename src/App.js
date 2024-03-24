f159faede9bad1b1b7b2d26406db96d389b1274f
Dej k <106344872+Dejuk@users.noreply.github.com>
2024-03-24 22:03:25 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ function App() {
       <Navbar />
       <SignUp/>
       <Routes> 
-
     <Route path='/' exact components ={Home} />
         <Route path='/services' components ={Services} />
         <Route path='/products' components ={Products} />
