81e227869cd5d97155a84eff4b8ee02ef07116f5
Dej k <106344872+Dejuk@users.noreply.github.com>
2024-03-24 22:03:40 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Navbar from './components/Navbar';
 import './App.css';
+
+
+
 import Home from './components/Pages/Home';
 import { BrowserRouter as Router, Route , Routes } from 'react-router-dom';
 import Services from './components/Pages/Services';
